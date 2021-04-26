0cd1be7b65adec9f7890085b96ccde99de049555
Takuya Shizukuishi <sititou70@gmail.com>
2021-04-26 17:50:36 +0900

    fix: XSS vulnerability

--- advanced/client/js/components/Todo.js
+++ advanced/client/js/components/Todo.js
@@ -1,22 +1,20 @@
 import { parseHTML } from '../lib/parseHTML.js';
 
 export const Todo = (todo) => {
-  const result = parseHTML(
+  const todo_item = parseHTML(
     `
     <li class="todo-item">
       <label class="todo-toggle__container">
-        <input data-todo-id="${
-          todo.id
-        }" type="checkbox" class="todo-toggle" value="${
-      todo.done ? 'checked' : ''
-    }">
+        <input data-todo-id="${todo.id}" type="checkbox" class="todo-toggle" value="checked">
         <span class="todo-toggle__checkmark"></span>
       </label>
-      <div class="todo-name">${todo.name}</div>
+      <div class="todo-name"></div>
       <div data-todo-id="1" class="todo-remove-button">x</div>
     </li>
     `
   );
 
-  return result;
+  todo_item.querySelector('.todo-name').innerText = todo.name;
+
+  return todo_item;
 };
